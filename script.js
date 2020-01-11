de9cedfd547cece13e75cfdf278bcd64709425a9
Hung Dinh Nguyen <hungnguyendinh1999@gmail.com>
2020-01-11 17:53:08 -0500

    Added character split

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-const RANDOM_QUOTABLE_API_URL = 'https://api.quotable.io/random'
+const RANDOM_QUOTABLE_API_URL = 'https://api.quotable.io/random';
+const quoteDisplayElement = document.getElementById('quoteDisplay');
+const quoteInputElement = document.getElementById('quoteIn');
+
+quoteInputElement.addEventListener('input', () => {
+    // console.log('CHANGES MADE')
+    const arrQuote = quoteDisplayElement.querySelectorAll('span');
+    
+})
 
 function getRandomQuote(){
-    fetch(RANDOM_QUOTABLE_API_URL) // fetch from quotable and return Promises
+    return fetch(RANDOM_QUOTABLE_API_URL) // fetch from quotable and return Promises
         .then(response => response.json()) //convert to json
         .then(data => data.content)
 }
 
-async function getNextQuote() { //async: a fnc always return a Promise
-    const quote = await getRandomQuote // wait until the promise resolves (*)
-    
+async function renderNewQuote() { //async: a fnc always return a Promise
+    const quote = await getRandomQuote(); // wait until the promise resolves (*)
+    quoteDisplayElement.innerHTML = '';
+    // Loop thru each individual Character of quoteDisplay
+    quote.split('').forEach(character => {
+        // each span is an individual character
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = character;
+
+        quoteDisplayElement.appendChild(characterSpan);
+    });
+    quoteInputElement.innerText = null;
 }
+
+renderNewQuote();
