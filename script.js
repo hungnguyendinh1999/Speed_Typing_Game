cf2f33345627c803e8803a44bffc7f7d958694ee
Hung Dinh Nguyen <hungnguyendinh1999@gmail.com>
2020-01-11 19:00:21 -0500

    Added Timer& Finish product

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const RANDOM_QUOTABLE_API_URL = 'https://api.quotable.io/random';
 const quoteDisplayElement = document.getElementById('quoteDisplay');
 const quoteInputElement = document.getElementById('quoteIn');
+let timerElement = document.getElementById('timer');
 
 quoteInputElement.addEventListener('input', () => {
     // console.log('CHANGES MADE')
     const arrQuote = quoteDisplayElement.querySelectorAll('span'); // choose all splitted 'span'
     const arrValue = quoteInputElement.value.split(''); // split values of input
     // how does the machine knows these parameters' types??? -> ((HTMLspan, number, NodeList parent) ->...)
+    let correct = true;
     arrQuote.forEach((characterSpan, index) => {
         const character = arrValue[index];
         if (character == null) {
             characterSpan.classList.remove('correct');
             characterSpan.classList.remove('incorrect');
+            correct = false;
         }
+        // === strictly equal
         else if (character === characterSpan.innerText) {
             characterSpan.classList.add('correct');
             characterSpan.classList.remove('incorrect');
@@ -20,11 +24,11 @@ quoteInputElement.addEventListener('input', () => {
         else {
             characterSpan.classList.add('incorrect');
             characterSpan.classList.remove('correct');
+            correct = false;
         }
-
     });
-    
-})
+    if (correct) renderNewQuote();
+});
 
 function getRandomQuote(){
     return fetch(RANDOM_QUOTABLE_API_URL) // fetch from quotable and return Promises
@@ -40,10 +44,23 @@ async function renderNewQuote() { //async: a fnc always return a Promise
         // each span is an individual character <span>
         const characterSpan = document.createElement('span');
         characterSpan.innerText = character;
-        // shouldn't we add sth here?
         quoteDisplayElement.appendChild(characterSpan);
     });
-    quoteInputElement.innerText = null;
+    quoteInputElement.value = null;
+    startTimer();
+}
+
+let startTime;
+function startTimer() {
+    timerElement.innerText = 0;
+    startTime = new Date(); // set to current time/date
+    setInterval(() => {
+        timer.innerText = getTimerTime();
+    }, 1000); // in miliseconds
+}
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000)
 }
 
 renderNewQuote();
